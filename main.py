80160b3007780a5bb0f2299fb575139ab231eab4
julioferreira2022 <108654089+julioferreira2022@users.noreply.github.com>
2023-06-01 00:44:02 -0300

    creates tests and user

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def main():
-    print("Hello, world!")
+from users import Users
 
 if __name__ == "__main__":
-    main()
+    print("Hello, this is a user management system")
+    user = Users()
+    user.create_user("joe","2014","2013@email.com","2014-03-10")
+    user.edit_user(1,"newjoe")
+    print(user.users_list[0])
---
+++ users.py
@@ -0,0 +1,42 @@
+class UserNotFoundException(Exception):
+    pass
+
+
+class Users(object):
+    def __init__(self, users_list=[]):
+        self.users_list = users_list
+        self.current_id = 0
+
+    def generate_id(self):
+        self.current_id += 1
+        return self.current_id
+
+    def create_user(self, username, password, email, birth_date):
+        user = {
+            'id': self.generate_id(),
+            'username': username,
+            'password': password,
+            'email': email,
+            'birth_date': birth_date
+        }
+        self.users_list.append(user)
+
+    def delete_user(self, user_id):
+        for user in self.users_list:
+            if user['id'] == user_id:
+                self.users_list.remove(user)
+                return
+
+    def edit_user(self, user_id, new_username=None, new_password=None, new_email=None, new_birth_date=None):
+        for user in self.users_list:
+            if user['id'] == user_id:
+                if new_username:
+                    user['username'] = new_username
+                if new_password:
+                    user['password'] = new_password
+                if new_email:
+                    user['email'] = new_email
+                if new_birth_date:
+                    user['birth_date'] = new_birth_date
+                return
+        raise UserNotFoundException(f"User with id {user_id} not found")
