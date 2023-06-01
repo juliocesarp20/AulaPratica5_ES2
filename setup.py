5c60bad110a6ff0d80367fe293943cc84a5b58e1
julioferreira2022 <108654089+julioferreira2022@users.noreply.github.com>
2023-06-01 02:13:16 -0300

    Revert "refactored exception names"
    This reverts commit 4960278f8c48ff125c50efaa147b623e7f30cfb7.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="user-management-package",
-    version="1.0.2",
+    version="1.0.1",
     packages=find_packages(),
     install_requires=[
         "ruff",
